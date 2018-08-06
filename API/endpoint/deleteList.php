7f578d6d900d178f4467eb5c0b08023a325a1db7
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2018-08-06 13:43:55 +0200

    Fix delete list bug.
    List was being deleted, but tasks remained in db.

--- API/endpoint/deleteList.php
+++ API/endpoint/deleteList.php
@@ -9,10 +9,12 @@ if(isset($_SESSION["user"]) && isset($_GET["listname"])) {
     $listname = $conn->real_escape_string($_GET["listname"]);
 
 
-    $sql = "DELETE FROM list WHERE listname='$listname'";
+    $sql = "DELETE FROM list WHERE listname='$listname';";
     $result = $conn->query($sql);
 
     if($conn->affected_rows === 1) {
+        $sql = "DELETE FROM task WHERE listname='$listname';"
+        $conn->query($sql);
         echo json_encode(array("success" => "List deleted"));
     } else {
         echo json_encode(array("error"=>"Failed to delete list..."));
